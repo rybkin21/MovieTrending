c7d8a73eb4615cb0c62948e9d5ccd4d2eb5b00c9
Ivan Rybkin <i.rybkin@icloud.com>
2024-10-12 15:37:33 +0300

    add model

---
+++ MovieTrending/MainViewController + TableView.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController + TableView.swift
+//  MovieTrending
+//
+//  Created by Ivan Rybkin on 10.10.2024.
+//
+
+import Foundation
+import UIKit
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        viewModel.numberOfSections()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfRows(in: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
